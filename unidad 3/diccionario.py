f83bfa49f20f512a72059a6b6cbbffa1bcfc2d20
Reynaj2931 <calzadareyna5@gmail.com>
2021-11-04 11:59:14 -0600

    diccionario.py

---
+++ unidad 3/diccionario.py
@@ -0,0 +1,48 @@
+Usuario = {
+   "nombre": "yamile",
+   "apellido": "campos",
+   "correo": "calazadareyna@gmail.com",
+}
+
+print(Usuario["correo"])
+
+Vestido = {
+      "color": "rosa",
+      "talla": "mediana",
+      "marca": "bcbg",
+}
+
+print(Vestido["color"])
+
+Troca = { 
+     "marca": "for f-150",
+     "color" : "negra",
+     "año" : "2021",
+}
+
+print(Troca["marca"])
+
+Celular =  {
+       "marca" : "motorola moto g100",
+       "color" : "blanco",
+       "tamaño": "grande",
+}
+
+print(Celular["tamaño"])
+
+Tacones =  {
+       "calzado": "3",
+       "color" : "azul",
+       "marca" : "steve madden carrson",
+}
+
+print(Tacones["marca"])
+
+Estudiante  = {
+      "nombre" : "jaime",
+      "apellido": "valadez",
+      "grado" : "4 semestre",
+}
+
+print(Estudiante["grado"])
+
