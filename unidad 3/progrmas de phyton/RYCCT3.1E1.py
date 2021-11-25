675f3cd55b1ba9ff8f070cbc4870a9bb1c73f973
Reynaj2931 <calzadareyna5@gmail.com>
2021-11-24 21:03:46 -0600

    numerosfibocc

---
+++ unidad 3/progrmas de phyton/RYCCT3.1E1.py
@@ -0,0 +1,12 @@
+Calificaciones = float(input('arrojame las calificacione:C1,C2,C3,C4,C5:'))
+
+Cal=float(Calificaciones)
+
+if Cal >=C1: 
+    print('C1,C2,C3,C4,C5')
+    print('las cinco Calificaciones')
+    
+else:
+    print('Calificaciones del semestre')
+    
+print('fin')
