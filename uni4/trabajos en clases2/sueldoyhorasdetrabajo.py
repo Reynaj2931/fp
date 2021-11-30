2ac5457fcbe880f213570603b3e47d54764a78a5
Reynaj2931 <calzadareyna5@gmail.com>
2021-11-29 20:27:26 -0600

    trabajosdeclase

---
+++ uni4/trabajos en clases2/sueldoyhorasdetrabajo.py
@@ -0,0 +1,7 @@
+
+nombre=(input("ingresa el nombre del trabajdor:"));
+sueldo=float(input("cual el sueldo del trabajdor:"));
+horas=float(input("cuantas horas del dia trabaja:"));
+trabajador= sueldo * horas
+print("el sueldo  del trabajador es de :" , trabajador)
+
