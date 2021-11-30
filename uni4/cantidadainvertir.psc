77502246d50fa14905c034179e86a859aa1a36ab
Reynaj2931 <calzadareyna5@gmail.com>
2021-11-29 20:12:47 -0600

    trabajo en clases2

---
+++ uni4/cantidadainvertir.psc
@@ -0,0 +1,16 @@
+Algoritmo cantidadainvertir
+	Definir  cantidadinvertir Como Entero
+	Escribir "ingresa la cantidad a invertir"
+	leer cantidad
+	Escribir "ingresa el interes anual"
+	leer interes
+	Escribir "ingresa años invertidos"
+	leer años
+	i = interes/100
+	a = cantidad* i
+	c =  cantidad+ c
+	t = a* años
+	imprimir "la cantidad invertir final es: $", t
+	imprimir "final"
+	
+FinAlgoritmo
---
+++ uni4/diasyropa.py
@@ -0,0 +1,53 @@
+###proyecto 1 de la unidad 4
+###hacer un programa en el que me diga que ropa me puedo poner cada dia de la semana 
+##reyna yamile calzada campos 
+
+dias=input("ingresa un dia de la semana:");
+vestimenta=input("elige una vestimenta para ponertela :");
+
+d1=("este lunes toca ponerme");
+v1=("un vestido");
+zap1=("tacones negros");
+print(d1);
+print(v1);
+print(zap1);
+
+dias=input("ingresa un dia de la semana:");
+d2=("este martes toca ponerme");
+vestimenta=input("elige una vestimenta para ponertela :");
+f2=(" una falda");
+vestimenta=input("elige una vestimenta para ponertela :");
+zap2=("unos huaraches");
+print(d2);
+print(f2);
+print(zap2);
+
+dias=input("ingresa un dia de la semana:");
+d3=("este miercoles toca ponerme");
+vestimenta=input("elige una vestimenta para ponertela :");
+p3=("un pants");
+zap3=("unos tenis");
+print(d3);
+print(p3);
+print(zap3);
+
+dias=input("ingresa un dia de la semana:");
+d4=("este jueves toca ponerme");
+vestimenta=input("elige una vestimenta para ponertela :");
+j4=("un jumper");
+zapatos=()
+zap1=("pocion1:tenis");
+zap2=("opcion2:huaraches");
+print(d4);
+print(j4);
+print(zap1);
+print(zap2);
+
+dias=input("ingresa un dia de la semana:");
+d5=("este viernes toca ponerme");
+vestimenta=input("elige una vestimenta para ponertela :");
+pb5=("un pantalon de mesclilla y una blusa");
+zap5=(" unos tacones azules");
+print(d5);
+print(pb5);
+print(zap5);
