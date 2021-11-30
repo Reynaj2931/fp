753eb01a00c372efd921f39f46b01b01ee3e74be
Reynaj2931 <calzadareyna5@gmail.com>
2021-11-29 20:20:41 -0600

    trabajoenclases

--- uni4/cantidadainvertir.py
+++ uni4/cantidadainvertir.py
@@ -1,5 +1,5 @@
 
 cantidad=float(input("cuanto es la cantidad de invertir:"))
 interes=float(input("cual es el interes anual:"))
-años=int(input("cuantos años:"))
+años=int(input("cuantos años son:"))
 print("capital obtenida es :" + str(round(cantidad *(interes/100) + años)))
--- uni4/sueldoyhorasdetrabajo.py
+++ uni4/sueldoyhorasdetrabajo.py
@@ -3,5 +3,5 @@ nombre=(input("ingresa el nombre del trabajdor:"));
 sueldo=float(input("cual el sueldo del trabajdor:"));
 horas=float(input("cuantas horas del dia trabaja:"));
 trabajador= sueldo * horas
-print("el sueldo es de :" , trabajador)
+print("el sueldo  del trabajador es de :" , trabajador)
 
