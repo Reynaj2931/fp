e9c75a49c5cbcbe0308b1fd523284113c0793160
Reynaj2931 <calzadareyna5@gmail.com>
2021-12-09 19:43:55 -0600

    programas final

---
+++ uni4/trabajos en clases2/calculadora.py
@@ -0,0 +1,26 @@
+print("seleccion una opcion:1 suma,2 resta,3 multiplicacion, 4 division")
+opci=int(input("selecciona una opcion:"))
+ifopc1==1:
+n1=input("ingresa el primer numero:");
+n2=input("ingresa el segundo numero:");
+s=(n1+n2)
+print(n1);
+print(n2);
+
+elif opci2==2:
+n1=input("ingresa el primer numero:");
+n2=input("ingresa el segundo numero:");
+r=(n1-n2)
+print("el resultado de la resta es:", r)
+
+elif opci3==3:
+n1=input("ingresa el primer numero:");
+n2=input("ingresa el segundo numero:");
+m=(n1*n2)
+print("el resultado de la multiplicacion es:",m)
+
+elif opci4==4:
+n1=input("ingresa el primer numero:");
+n2=input("ingresa el segundo numero:");
+d=(n1/n2)
+print("el resultado de la division es:",d);
---
+++ uni4/trabajos en clases2/materiaycreditos.py
@@ -0,0 +1,14 @@
+##selecciona un semestre :1 semestre,2 semeestre y 3 semestre
+##semestre1
+sem0=int(input("en que semestre cursas:"))
+sem1=("el semestre vale:26")
+
+ if sem1=int(input("cuales son las materias que cursas:"))
+c=input("calculo")
+te=input("taller de etica")
+md=input("matematicas discretas")
+pg=imput("programacio")
+tcs=input("tics")
+igl=input("ingles")
+c=int(c)+int(te)+int(md)+int(pg)+int(tcs)+int(igl)
+print("los creditos son:",26)
