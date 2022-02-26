c79dcb896fd8db0b0af64be9005153ec3dddb36b
Reynaj2931 <calzadareyna5@gmail.com>
2022-02-26 13:00:54 -0600

    examenunidad1

---
+++ segundo semestre_unidad1/examenUni1.py
@@ -0,0 +1,53 @@
+print("entra a la pagina")
+cont = input("Dime la contaseña:")
+usu = input("Dime tu usuario:")
+if usu == "reyna" or cont == "r":
+    print("bienvenido")
+else:
+    print("Contraseña incorrecta")
+    print("segunda oportunidad")
+    cont = input("Dime la contaseña:")
+    usu= input("Dime tu usuario:")
+    if usu == "reyna" or cont == "r":
+        print("bienvenido")
+    else:
+        print("Contraseña incorrecta")
+        print("tercera oportunidad")
+        cont= input("Dime la contaseña:")
+        usu= input("Dime tu usuario:")
+        if usu== "reyna" or cont == "r":
+            print("bienvenido")
+        else:
+            print("Contraseña incorrecta")
+            print("te quedan 0 intentos")
+            print("Espera 2 minutos")
+            print(".")
+            print(".")
+            print("tienes 3 intentos")
+            usu= input("Dime tu usuario:")
+            cont = input("ingresa la contaseña") 
+            if usu == "reyna" or cont == "r":
+                print("bienvenido")
+            else:
+                print("Contraseña incorrecta")
+                print("segunda oportunidad")
+                cont= input("Dime la contaseña:")
+                usu= input("Dime tu usuario")
+                if usu == "reyna" or cont == "r":
+                    print("bienvenido")
+                else:
+                    print("Contraseña incorrecta")
+                    print("tercera oportunidad")
+                    cont= input("Dime la contaseña:")
+                    usu= input("Dime tu usuario:")
+                    if usu == "reyna" or cont == "r":
+                        print("bienvenido")
+                    else:
+                        print("Contraseña incorrecta")
+                        print("Sliendo del sistema")
+
+        
+            
+       
+
+
