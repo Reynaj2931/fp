91a57cde64d86e535ef61c1856ad6f0097ddeadf
reyna <calzadareyna5@gmail.com>
2022-05-12 16:27:17 -0500

    programas de lista y arreglos

---
+++ unidad 4y 5/programas de arregos y diccionario/diccioario.py
@@ -0,0 +1,125 @@
+animales1_diccio = {
+    "nombre del animal" :"gato",
+    "color":"blanco",
+    "edad":"4 años"
+}
+
+print(animales1_diccio["nombre del animal"])
+print(animales1_diccio["color"])
+print(animales1_diccio["edad"])
+
+print("--------------------------------------")
+animales2_diccio= {
+    "nombre del animal1" :"perro",
+    "color":"negro",
+    "edad":"2 años"
+    
+}
+
+print(animales2_diccio["nombre del animal1"])
+print(animales2_diccio["color"])
+print(animales2_diccio["edad"])
+print("--------------------------------------")
+
+animales3_diccio= {
+    "nombre del animal3" :"pajaro",
+    "color":"negro con pecho rojo",
+    "tamaño":"grande",
+}
+
+print(animales3_diccio["nombre del animal3"])
+print(animales3_diccio["color"])
+print(animales3_diccio["tamaño"])
+print("--------------------------------------")
+
+animales4_diccio={
+    "nombre del animal4" :"vaca",
+    "color":"negro con blanco",
+    "tamaño":"grande",
+    "alimento": "paja de cereales y afalfa",
+}
+
+print(animales4_diccio["nombre del animal4"])
+print(animales4_diccio["color"])
+print(animales4_diccio["tamaño"])
+print(animales4_diccio["alimento"])
+print("--------------------------------------")
+
+
+persona1_diccio={
+    "nombre de la persona1" :"magali",
+    "apellido":"campos",
+    "edad":"42",
+    "lugar de nacimiento":"Aguascalientes",
+    "ocupacion": "ama de casa",
+}
+
+print(persona1_diccio["nombre de la persona1"])
+print(persona1_diccio["apellido"])
+print(persona1_diccio["edad"])
+print(persona1_diccio["lugar de nacimiento"])
+print(persona1_diccio["ocupacion"])
+print("--------------------------------------")
+
+persona2_diccio={
+    "nombre de la persona2" :"pedro",
+    "apellido":"Ramirez",
+    "edad":"45",
+    "lugar de nacimiento":"Aguascalientes",
+    "ocupacion": "obrero",
+}
+
+print(persona2_diccio["nombre de la persona2"])
+print(persona2_diccio["apellido"])
+print(persona2_diccio["edad"])
+print(persona2_diccio["lugar de nacimiento"])
+print(persona2_diccio["ocupacion"])
+print("--------------------------------------")
+
+persona3_diccio={
+    "nombre de la persona3" :"antonieta",
+    "apellido":"garcia",
+    "edad":"34",
+    "lugar de nacimiento":"Aguascalientes",
+    "ocupacion": "empleada de hogar",
+}
+
+print(persona3_diccio["nombre de la persona3"])
+print(persona3_diccio["apellido"])
+print(persona3_diccio["edad"])
+print(persona3_diccio["lugar de nacimiento"])
+print(persona3_diccio["ocupacion"])
+print("--------------------------------------")
+
+persona4_diccio={
+    "nombre de la persona4" :"jaime",
+    "apellido":"valadez",
+    "edad":"18",
+    "lugar de nacimiento":"Aguascalientes",
+    "ocupacion": "musico",
+}
+
+print(persona4_diccio["nombre de la persona4"])
+print(persona4_diccio["apellido"])
+print(persona4_diccio["edad"])
+print(persona4_diccio["lugar de nacimiento"])
+print(persona4_diccio["ocupacion"])
+print("--------------------------------------")
+
+persona5_diccio={
+    "nombre de la persona5" :"jenni",
+    "apellido":"calzada",
+    "edad":"20",
+    "lugar de nacimiento":"Aguascalientes",
+    "ocupacion": "obrera de fabrica",
+}
+
+print(persona5_diccio["nombre de la persona5"])
+print(persona5_diccio["apellido"])
+print(persona5_diccio["edad"])
+print(persona5_diccio["lugar de nacimiento"])
+print(persona5_diccio["ocupacion"])
+print("--------------------------------------")
+
+
+
---
+++ unidad 4y 5/programas de arregos y diccionario/listasyarreglos1.py
@@ -0,0 +1,20 @@
+Nombres = ["magali","jenni","jaime","pedro"]
+print(Nombres)
+Colores = ["rosa","azul","verde","morado"]
+print(Colores)
+Ropa = ["pantalon","ombligeras","vestido","short"]
+print(Ropa)
+Vocales =["a","e","i","o","u"]
+print(Vocales)
+Materias=["matematicas","español","ingles","historia"]
+print(Materias)
+Animales =["conejo","perro","mariposa","pajaro"]
+print(Animales)
+Marcas=["samsung","motorola","apple","zte"]
+print(Marcas )
+objetos=["escoba","mesa","cama","ropero"]
+prit(objetos)
+plantas=["helecho","cactus","palma bambu","azalea"]
+print(plantas)
+instrumentos =["guitarra","congas","tarolas","trompeta"]
+print(instrumentos)
